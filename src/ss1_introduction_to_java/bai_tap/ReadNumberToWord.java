4de56b826245b89f3a92171f4de1690ab0ba81b7
doducuy <doducuyktdn@gmail.com>
2022-08-01 20:42:33 +0700

    nop bai

--- src/ss1_introduction_to_java/bai_tap/ReadNumberToWord.java
+++ src/ss1_introduction_to_java/bai_tap/ReadNumberToWord.java
@@ -4,72 +4,36 @@ import java.util.Scanner;
 
 public class ReadNumberToWord {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int num;
-        char numOne = 0;
-        char numTwo = 0;
-        System.out.print("input number: ");
-        num =scanner.nextInt();
-        if(num>=20&& num <100){
-            String numString = Integer.toString(num);
-            numOne= numString.charAt(0);
-            numTwo = numString.charAt(1);
+        String[] numbersUnits = {" ", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] numbersTens = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        String[] numString = Integer.toString(number).split("", 0);
+        switch (numString.length) {
+            case 1:
+            case 2:
+                if (number == 0) {
+                    System.out.println("Zero");
+                } else if (number < 20) {
+                    System.out.print(numbersUnits[number]);
+                } else {
+                    System.out.print(numbersTens[Integer.parseInt(numString[0]) - 2] + ' ' + numbersUnits[Integer.parseInt(numString[1])]);
+                }
+                break;
+            case 3:
+                System.out.print(numbersUnits[Integer.parseInt(numString[0])] + " hundred and ");
+                number -= Integer.parseInt(numString[0]) * 100;
+                if (number == 0) {
+                    System.out.println("Zero");
+                } else if (number < 20) {
+                    System.out.print(numbersUnits[number]);
+                } else {
+                    System.out.print(numbersTens[Integer.parseInt(numString[1]) - 2] + ' ' + numbersUnits[Integer.parseInt(numString[2])]);
+                }
+                break;
+            default:
+                System.out.println("out of ability");
         }
-        switch (numOne){
-            case '2':
-                System.out.print("twenty ");
-                break;
-            case '3':
-                System.out.print("thirty ");
-                break;
-            case '4':
-                System.out.print("fourty ");
-                break;
-            case '5':
-                System.out.print("fivety ");
-                break;
-            case '6':
-                System.out.print("sixty ");
-                break;
-            case '7':
-                System.out.print("seventy ");
-                break;
-            case '8':
-                System.out.print("eighty ");
-                break;
-            case '9':
-                System.out.print("ninety ");
-                break;
-        }
-        switch (numTwo){
-            case '1':
-                System.out.println("one");
-                break;
-            case '2':
-                System.out.print("two");
-                break;
-            case '3':
-                System.out.print("three");
-                break;
-            case '4':
-                System.out.print("four");
-                break;
-            case '5':
-                System.out.print("five");
-                break;
-            case '6':
-                System.out.print("six");
-                break;
-            case '7':
-                System.out.print("seven");
-                break;
-            case '8':
-                System.out.print("eight");
-                break;
-            case '9':
-                System.out.print("night");
-                break;
-        }
-
     }
 }
--- src/ss1_introduction_to_java/bai_tap/ShowGreeting.java
+++ src/ss1_introduction_to_java/bai_tap/ShowGreeting.java
@@ -1,11 +1,13 @@
 package ss1_introduction_to_java.bai_tap;
+
 import java.util.Scanner;
+
 public class ShowGreeting {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String name;
         System.out.print("input name: ");
-        name= scanner.next();
-        System.out.printf("Hello %s" ,name);
+        name = scanner.next();
+        System.out.printf("Hello %s", name);
     }
 }
