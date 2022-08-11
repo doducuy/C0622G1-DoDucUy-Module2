6397ac7480e1c1edc149356b4723ad4f46224d7d
doducuy <doducuyktdn@gmail.com>
2022-08-11 20:43:49 +0700

    nop bai

---
+++ src/ss10_dsa_list/exercise/controller/Main.java
@@ -0,0 +1,25 @@
+package ss10_dsa_list.exercise.controller;
+
+import ss10_dsa_list.exercise.model.Mylist;
+
+public class Main {
+    public static void main(String[] args) {
+        Mylist<Integer> list = new Mylist<>();
+        list.add(4);
+        list.add(3);
+        list.add(5);
+        list.add(5);
+        list.remove(2);
+        Mylist<Integer> list1 = (Mylist<Integer>)list.clone();
+        System.out.println(list1);
+        for (int i = 0; i < list.getSize() ; i++) {
+            System.out.print(list.get(i)+"\t");
+        }
+
+        System.out.println();
+        System.out.println(list.indexOf(5));
+        list.clear();
+        System.out.println(list);
+
+    }
+}
