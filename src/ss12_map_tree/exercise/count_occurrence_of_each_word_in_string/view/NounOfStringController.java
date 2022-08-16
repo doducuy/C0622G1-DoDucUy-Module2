54cfeb54fc5162990f1b2c9d1465e757348e872b
doducuy <doducuyktdn@gmail.com>
2022-08-17 06:25:58 +0700

    nop bai

---
+++ src/ss12_map_tree/exercise/count_occurrence_of_each_word_in_string/view/NounOfStringController.java
@@ -0,0 +1,15 @@
+package ss12_map_tree.exercise.count_occurrence_of_each_word_in_string.view;
+
+import ss12_map_tree.exercise.count_occurrence_of_each_word_in_string.model.NounOfString;
+
+import java.util.Scanner;
+
+public class NounOfStringController {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập 1 chuỗi: ");
+        String string = sc.nextLine();
+        NounOfString.checkNounOfString(string);
+    }
+}
+
